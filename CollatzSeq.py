52f1802092816f4c00819a2a638c4c1f66f07b42
Bikash Das <dasbikash989@gmail.com>
2019-05-20 09:32:14 +0530

    Added code for Collatz Sequence

--- CollatzSeq.py
+++ CollatzSeq.py
@@ -1,1 +1,15 @@
 # program for Collatz Sequence
+
+def collatz(number):
+    if(number % 2 == 0):
+        print(number // 2)
+        return number // 2
+    else:
+        print(3 * number + 1)
+        return 3 * number + 1
+def main():
+    n = int(input("Enter a number: "))
+    n = collatz(n)
+    while(n != 1):
+        n = collatz(n)
+main()
