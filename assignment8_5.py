3c57182570fb3597e11a26032dbb94c22f75a669
Bikash Das <dasbikash989@gmail.com>
2019-05-23 21:42:27 +0530

    added exception

--- assignment8_5.py
+++ assignment8_5.py
@@ -1,13 +1,16 @@
-fname = input("Enter file name: ")
-if len(fname) < 1 : fname = "mbox-short.txt"
-count = 0
-fh = open(fname)
-for line in fh:
-    if not line.startswith('From'):
-        continue
-    words = line.split()
-    print(words[1])
-    count += 1
+try:
+    fname = input("Enter file name: ")
 
+    count = 0
+    fh = open(fname)
+    for line in fh:
+        if not line.startswith('From'):
+            continue
+        words = line.split()
+        print(words[1])
+        count += 1
 
-print("There were", count, "lines in the file with From as the first word")
+    print("There were", count, "lines in the file with From as the first word")
+
+except Exception as e:
+    print('error')
