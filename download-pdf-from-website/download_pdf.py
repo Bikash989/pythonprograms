76afe40fd5659e45805590dc8024bb455092e0c0
Bikash Das <dasbikash989@gmail.com>
2019-06-03 22:25:12 +0530

    Added exception, now python will prompt for a filename

--- download-pdf-from-website/download_pdf.py
+++ download-pdf-from-website/download_pdf.py
@@ -4,28 +4,24 @@ import requests
 # need to modify find-pdf-link-in-webpage.property
 #will do it later
 #for now assuming the file contains full path
+fname = input("Enter file name: ")
+try:
+    fh = open(fname,'r')
+    for line in fh:
+        link = line.strip()
+        # print('link: ',link)
+        r = requests.get(link, stream=True)
+        chunk_size = 2000
 
-fh = open('cs229PdfLinks.txt','r')
-for line in fh:
-    link = line.strip()
-    # print('link: ',link)
-    r = requests.get(link, stream=True)
-    chunk_size = 2000
-
-    #file name
-    words = link.split('/')
-    fname = words[len(words)-1]
-    print('Downloading file from ', link)
-    with open(fname,'wb') as fd:
-        for chunk in r.iter_content(chunk_size):
-            fd.write(chunk)
-    print("Done.")
-print('\nDownloaded')
-fh.close()
-
-# url = 'http://www.hrecos.org//images/Data/forweb/HRTVBSH.Metadata.pdf'
-# r = requests.get(url, stream=True)
-# chunk_size = 2000
-# with open('mypdf.pdf', 'wb') as fd:
-#     for chunk in r.iter_content(chunk_size):
-#         fd.write(chunk)
+        #file name
+        words = link.split('/')
+        fname = words[len(words)-1]
+        print('Downloading file from ', link)
+        with open(fname,'wb') as fd:
+            for chunk in r.iter_content(chunk_size):
+                fd.write(chunk)
+        print("Done.")
+    print('\nDownloaded')
+    fh.close()
+except Exception as e:
+    print(e)
