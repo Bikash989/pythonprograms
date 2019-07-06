f8335e5b62d3881899302847f25bc0a3e9fe17a5
Bikash Das <dasbikash989@gmail.com>
2019-07-06 17:06:25 +0530

    In tab3, explored canvas widget

---
+++ tkinter/ch-3/canvasWidget.py
@@ -0,0 +1,68 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import Spinbox
+from tkinter import scrolledtext
+win = tk.Tk()
+win.title("Tabbed Widgets")
+
+
+
+tabControl = ttk.Notebook(win) # creates tabl
+
+tab1 = ttk.Frame(tabControl)
+tabControl.add(tab1, text = "Tab 1")
+tab2 = ttk.Frame(tabControl)
+tabControl.add(tab2, text = "Tab 2")
+
+tabControl.pack(expand = 1, fill = "both") # pack to make visible
+
+'''
+hierarchy, tab -> labelframe -> labelInside
+'''
+# make a label Frame
+mighty = ttk.LabelFrame(tab1, text = "Mighty Python")
+mighty.grid(column = 0, row = 0, padx = 8, pady = 20)
+
+# label using mighty as parent
+my_label = ttk.Label(mighty, text = "Enter a name:")
+my_label.grid(column = 0, row = 0, padx = 20, pady = 20, sticky = 'W')
+val = tk.StringVar()
+tk.Entry(mighty, textvariable = val).grid(column = 1, row = 0, padx = 10, pady = 10, sticky = 'W')
+
+# spinbox callback
+def _spin():
+    value = spin.get()
+    print(value)
+    scrol.insert(tk.INSERT, value + '\n')
+
+# adding a spinbox Widget
+spin = Spinbox(mighty, from_ = 0, to = 10, width = 5, bd = 8, command = _spin)
+spin.grid(column = 0, row = 2, padx = 10, pady = 10)  # bd-border width
+
+scrol = scrolledtext.ScrolledText(mighty, height = 10, width = 40, wrap = tk.WORD)
+scrol.grid(row = 3, columnspan = 4)
+
+my_label2 = ttk.Label(mighty, text = "Choose a number")
+my_label2.grid(column = 0, row = 1, padx = 20, pady = 20, sticky = 'W', )
+
+comboval = tk.StringVar()
+combo = ttk.Combobox(mighty, textvariable = comboval, state = 'readonly')
+combo['values'] = (1,2,3,4,5)
+combo.grid(column = 1, row = 1, padx = 14, pady = 20, sticky = 'W')
+combo.current(0)
+
+# adding in tab2
+about = ttk.LabelFrame(tab2, text = "About")
+about.grid(column = 0, row = 0, padx = 8, pady = 20)
+ttk.Label(about, text = "Hello it's me bikash das").grid(padx = 20, pady = 10,column = 0, row = 0)
+
+
+# add a third tab
+tab3 = ttk.Frame(tabControl)
+tabControl.add(tab3, text = 'Canvas')
+tab3Frame = tk.Frame(tab3, bg = 'blue')
+tab3Frame.pack()
+for orange_color in range(2):
+    canvas = tk.Canvas(tab3Frame, width = 150, height = 80, highlightthickness = 0, bg = 'orange')
+    canvas.grid(row = orange_color, column = orange_color)  # coordinate 0,0 and 1,1
+win.mainloop()
