05b1d3c6d70dd5280e200e4f22e8f98d774a2293
Bikash Das <dasbikash989@gmail.com>
2019-07-06 17:08:51 +0530

    some changes

--- tkinter/ch-3/independentMessageBox.py
+++ tkinter/ch-3/independentMessageBox.py
@@ -2,5 +2,5 @@ from tkinter import messagebox as msg
 from tkinter import Tk
 
 root = Tk()
-root.withdraw() # this removes the default window 
+root.withdraw() # this removes the default window
 msg.showinfo('This is a title', 'Python information pop up message box')
