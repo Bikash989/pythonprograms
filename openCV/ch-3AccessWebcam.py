8b81583ed6757fca24eea26bd05ccf7d78547469
Bikash Das <dasbikash989@gmail.com>
2019-09-09 16:48:43 +0530

    Capture Video using openCv and show it

---
+++ openCV/ch-3AccessWebcam.py
@@ -0,0 +1,14 @@
+import cv2
+cap = cv2.VideoCapture(0) #capture from camera
+if not cap.isOpened():
+    raise IOError("Cannot open webcam")
+while True:
+    ret, frame = cap.read()
+    # frame = cv2.resize(frame, None, fx=0.5, fy=0.5,
+    # interpolation=cv2.INTER_AREA)
+    cv2.imshow('Input',frame) 
+    c = cv2.waitKey(1)
+    if c == 27: #esc to quit
+        break
+cap.release()
+cv2.destroyAllWindows()
