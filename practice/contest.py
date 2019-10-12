ee0e43bb8866b7a5acf635574baa9f67de8ee439
Bikash Das <dasbikash989@gmail.com>
2019-10-13 03:41:22 +0530

    Second attempt

--- practice/contest.py
+++ practice/contest.py
@@ -1,46 +1,75 @@
-
-
 class ModList:
     def __init__(self, l):
         self.list = l
-    
     def append(self,val):
         self.list.append(val)
-            # list[i] = list[i] + val #no item assignment
-    
     
     def __add__(self,other):
-        sum = [] #empty list
-        for a,b in zip(self.list,other.list):
-            sum.append(a+b)
-        return sum
-    # overload __add__ how?
-    def __iadd__(self,other):
-        for i in range(len(self.list)):
-            self.list[i] += other
-        return self.list
+        if(type(self).__name__ == type(other).__name__):                
+            res = [] #empty list
+            for a,b in zip(self.list,other.list):
+                res.append(a+b)
+            return res    
+        else:
+            return ModList([val + other for val in self.list])
 
-    def __pos__(self):
-        for i in range(len(self.list)):
-            self.list[i] += other
-        return self.list
-        
+     # overloading - op
+    def __sub__(self,other):
+        if(type(self).__name__ == type(other).__name__):                
+            res = [] #empty list
+            for a,b in zip(self.list,other.list):
+                res.append(a - b)
+            return res    
+        else:
+            return ModList([val - other for val in self.list])
+             # overloading - op
+    def __mul__(self,other):
+        if(type(self).__name__ == type(other).__name__):                
+            res = [] #empty list
+            for a,b in zip(self.list,other.list):
+                res.append(a * b)
+            return res    
+        else:
+            return ModList([val * other for val in self.list])
+             # overloading - op
+    def __truediv__(self,other):
+        if(type(self).__name__ == type(other).__name__):                
+            res = [] #empty list
+            for a,b in zip(self.list,other.list):
+                res.append(a / b)
+            return res    
+        else:
+            return ModList([val / other for val in self.list])
+             # overloading - op
+    def __floordiv__(self,other):
+        if(type(self).__name__ == type(other).__name__):                
+            res = [] #empty list
+            for a,b in zip(self.list,other.list):
+                res.append(a // b)
+            return res    
+        else:
+            return ModList([val // other for val in self.list])
+    def __pow__(self,other):
+            if(type(self).__name__ == type(other).__name__):                
+                pass #return error   
+            else:
+                return ModList([val ** other for val in self.list])
 
     def __repr__(self):
         return '{}'.format(self.list)
-    
-    
 
-a = ModList([1,2,3,4])
-b = ModList([10,20])
-print(a,b)
-c = a+b
-print(c)
-a.append(20)
-b.append(30)
-b.append(40)
-print(a,b)
-print(a+b)
+
+a = ModList([10,25,60])
+b = ModList([2,6])
 print(a)
-a += 2
-print(a)
+print(a/b)
+print(a//b)
+print(a*b)
+print(a ** 2)
+
+# bonus part, unable to implement
+x = list()
+y = list()
+x.extend([1,2,3])
+y.extend([1,2,3])
+print(x+y)
